842552c882893c74276f92210a835f8918f8c61a
Aenitna <antineacaldara@gmail.com>
2023-04-04 10:05:18 +0200

    1

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int minimo = -1000;
+        int massimo = 1000;
+
+        //int[] arrayInteri = {4, 9, 12, 1, 3, 15};
+
+        List<Integer> listaInteri = Arrays.asList(4, 9, 12, 1, 3, 15);
+
+        System.out.println("Il minimo numero è: " + Collections.min(listaInteri));
+        System.out.println("Il massimo numero è: " + Collections.max(listaInteri));
+
+        for (int i = 0; i<listaInteri.size(); i++) {
+
+            
+
+        }
+
+    }
+
+}
